81e0a2a4312fb55d7dab7b4445c74b444c0ab9c8
unknown <f:\The Bat!>
2019-09-06 10:25:56 +0300

    first commit

---
+++ database/migrations/2019_08_31_150448_create_companies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 200);
+            $table->string('country', 100);
+            $table->string('city', 100);
+            $table->integer("user_id")->unsigned();
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+
+            $table->dropForeign(['user_id']);
+
+            $table->dropIfExists('companies');
+        });
+    }
+}
---
+++ database/migrations/2019_08_31_152423_create_vacancies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVacanciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacancies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('workers');
+            $table->integer('company_id')->unsigned();
+            $table->integer('salary');
+
+            $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vacancies', function (Blueprint $table) {
+
+            $table->dropForeign(['company_id']);
+
+            $table->dropIfExists('vacancies');
+        });
+    }
+}
